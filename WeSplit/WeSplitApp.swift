4bf033c56a6a483aba60093a80fb5d12b51d9904
Nicholas Verrico <nicholas.verrico@icloud.com>
2021-09-19 16:08:55 -0400

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Nicholas Verrico on 9/19/21.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
